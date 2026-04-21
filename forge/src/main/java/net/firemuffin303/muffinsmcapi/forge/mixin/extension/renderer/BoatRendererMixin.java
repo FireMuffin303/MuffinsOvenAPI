da8e1e025e6b372bc8ce7d735874b6f6847b5782
FireMuffin303 <firegame303@gmail.com>
2026-04-21 13:58:07 +0700

    snapshot-5 jitpack update

--- forge/src/main/java/net/firemuffin303/muffinsmcapi/forge/mixin/extension/renderer/BoatRendererMixin.java
+++ forge/src/main/java/net/firemuffin303/muffinsmcapi/forge/mixin/extension/renderer/BoatRendererMixin.java
@@ -1,7 +1,7 @@
 package net.firemuffin303.muffinsmcapi.forge.mixin.extension.renderer;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.datafixers.util.Pair;
 import net.firemuffin303.muffinsmcapi.impl.entity.boat.IOvenBoat;
 import net.firemuffin303.muffinsmcapi.impl.entity.boat.client.OvenBoatRenderer;
@@ -9,18 +9,20 @@ import net.minecraft.client.model.ListModel;
 import net.minecraft.client.renderer.entity.BoatRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.vehicle.Boat;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
+@Debug(export = true)
 @Mixin(BoatRenderer.class)
 public abstract class BoatRendererMixin {
 
-    @ModifyReturnValue(method = "getModelWithLocation",at = @At("RETURN"))
-    public Pair<ResourceLocation, ListModel<Boat>> muffins$redirectRendering(Pair<ResourceLocation, ListModel<Boat>> original, @Local(argsOnly = true) Boat boat){
+    @WrapOperation(method = "render(Lnet/minecraft/world/entity/vehicle/Boat;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/BoatRenderer;getModelWithLocation(Lnet/minecraft/world/entity/vehicle/Boat;)Lcom/mojang/datafixers/util/Pair;"))
+    public Pair<ResourceLocation, ListModel<Boat>> muffins$redirectRendering(BoatRenderer instance, Boat boat, Operation<Pair<ResourceLocation, ListModel<Boat>>> original){
         if(boat instanceof IOvenBoat ovenBoat && ((BoatRenderer)(Object)this) instanceof OvenBoatRenderer ovenBoatRenderer){
             return ovenBoatRenderer.getTextureAndModel(ovenBoat.getOvenBoatVariant().get());
         }
 
-        return original;
+        return original.call(instance, boat);
     }
 }
